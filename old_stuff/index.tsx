a02ead268444d26ece3be27236c90b3ef9d7f681
NickolasDesigns <nick.whitman@gmail.com>
2024-09-22 18:56:32 -0700

    npx initialization

--- old_stuff/index.tsx
+++ old_stuff/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Resume from "./components/resume/index";
+import Resume from "./resume/index";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
