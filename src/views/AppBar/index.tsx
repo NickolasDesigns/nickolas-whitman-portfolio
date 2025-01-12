14b69713d33435ee6a8393ee1ae91727eb01910c
NickolasDesigns <nick.whitman@gmail.com>
2025-01-11 16:14:25 -0800

    Removed Resume. Portfolio project is still needing a lot of work.

--- src/views/AppBar/index.tsx
+++ src/views/AppBar/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { Box, Tabs, Tab } from '@mui/material';
-import Resume from '../resume';
+// import Resume from '../resume';
 import PongGame from '../Pong';
 
 export function AppBar() {
@@ -23,11 +23,11 @@ export function AppBar() {
                     aria-label="scrollable force tabs example"
                     style={{ height: '20px' }}
                 >
-                    <Tab label="Resume" component={Link} to="/resume" />
+                    {/** <Tab label="Resume" component={Link} to="/resume" /> */}
                     <Tab label="Pong" component={Link} to="/pong" />
                 </Tabs>
                 <Routes>
-                    <Route path="/resume" element={<Resume />} />
+                    {/** <Route path="/resume" element={<Resume />} /> */}
                     <Route
                         path="/pong"
                         element={
